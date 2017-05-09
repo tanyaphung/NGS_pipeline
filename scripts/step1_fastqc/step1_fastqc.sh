e8bee450756bd31aed2e264d5d6d0384d13e03ee
tnphung <tnphung@alum.mit.edu>
2017-05-09 11:34:52 -0700

    Minor changes of variable name

--- scripts/step1_fastqc/step1_fastqc.sh
+++ scripts/step1_fastqc/step1_fastqc.sh
@@ -2,9 +2,9 @@
 #$ -cwd
 #$ -V
 #$ -l h_data=4G,time=24:00:00
-#$ -M eplau
+#$ -M phung428
 #$ -m ea
 
-individual=$1
+Individual=$1
 
-fastqc -o ../../analyses/step1_fastqc/${individual} ../../download/fastq/${individual}/*.fastq.gz
+fastqc -o ../../analyses/step1_fastqc ../../download/fastq/${Individual}/*.fastq.gz
